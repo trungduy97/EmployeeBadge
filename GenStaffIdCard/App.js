2cc05a1dc7f21e54355637bee046cfff061e8e91
duy <duongtrungduyit@gmail.com>
2019-04-07 17:42:40 +0700

    update

--- GenStaffIdCard/App.js
+++ GenStaffIdCard/App.js
@@ -6,41 +6,41 @@
  * @flow
  */
 
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  Dimensions,
-} from "react-native";
+  Dimensions
+} from 'react-native'
 
-import { Home } from "./Components/Home/Home";
+import { Home } from './Components/Home/Home'
 
-var { height, width } = Dimensions.get("window");
+import ImagePicker from 'react-native-image-picker'
+
+var { height, width } = Dimensions.get('window')
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu",
-});
-
-import ImagePicker from "react-native-image-picker";
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
+})
 
 // More info on all the options is below in the API Reference... just some common use cases shown here
 const options = {
-  title: "Select Avatar",
-  customButtons: [{ name: "fb", title: "Choose Photo from Facebook" }],
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
   storageOptions: {
     skipBackup: true,
-    path: "images",
-  },
-};
+    path: 'images'
+  }
+}
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Home />
@@ -49,47 +49,47 @@ export default class App extends Component {
           <Text>Button</Text>
         </TouchableOpacity> */}
       </View>
-    );
+    )
   }
 
-  show() {
+  show () {
     ImagePicker.showImagePicker(options, response => {
-      console.log("Response = ", response);
+      console.log('Response = ', response)
 
       if (response.didCancel) {
-        console.log("User cancelled image picker");
+        console.log('User cancelled image picker')
       } else if (response.error) {
-        console.log("ImagePicker Error: ", response.error);
+        console.log('ImagePicker Error: ', response.error)
       } else if (response.customButton) {
-        console.log("User tapped custom button: ", response.customButton);
+        console.log('User tapped custom button: ', response.customButton)
       } else {
-        const source = { uri: response.uri };
+        const source = { uri: response.uri }
 
         // You can also display the image using data:
         // const source = { uri: 'data:image/jpeg;base64,' + response.data };
 
         this.setState({
-          avatarSource: source,
-        });
+          avatarSource: source
+        })
       }
-    });
+    })
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#80d6ff",
+    alignItems: 'center',
+    backgroundColor: '#80d6ff'
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 10,
+    textAlign: 'center',
+    margin: 10
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-  },
-});
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+})
