c14347322dd584234c0b66333eb4065be73130d5
duy <duongtrungduyit@gmail.com>
2019-04-07 17:58:40 +0700

    update view home

--- GenStaffIdCard/Components/Home/Footer.js
+++ GenStaffIdCard/Components/Home/Footer.js
@@ -1,31 +1,50 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
-var { height, width } = Dimensions.get("window");
+import React, { Component } from 'react'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image
+} from 'react-native'
+var { height, width } = Dimensions.get('window')
 
 export class Footer extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.footer}>
+        <Image
+          style={styles.imageIcon}
+          source={require('../../img/iconhome.png')}
+        />
         <Text style={styles.text}>Home</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   footer: {
+    flexDirection: 'row',
     width: width,
     height: 35,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#80d6ff",
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#80d6ff'
   },
   text: {
-    justifyContent: "center",
-    alignItems: "center",
-    color: "white",
-    paddingTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    paddingTop: 15,
+    marginLeft: 120,
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
-});
+  imageIcon: {
+    marginTop: 15,
+    marginLeft: 15,
+    width: 35,
+    height: 35
+  }
+})
